8b4ad802188652791563d45573179158e5d4e69e
vsdiaman <vitor.diamantino8@gmail.com>
2022-02-05 19:34:18 -0300

    Projeto base e instalando as dependencias

---
+++ src/pages/telaInicial.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TelaInicial() {
+  return (
+    <div>
+      <h1 className="text-center">Simule seu investimento</h1>
+    </div>
+  );
+}
+
+export default TelaInicial;
